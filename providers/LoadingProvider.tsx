845dd0bc861f95897febb692e4ff046ce9cafbee
Naeem Sani <muhdnaeem95@gmail.com>
2024-07-12 11:20:26 +0800

    Added new provider for loading states.

---
+++ providers/LoadingProvider.tsx
@@ -0,0 +1,47 @@
+import React, { ReactNode, createContext, useContext, useEffect, useState } from 'react';
+import * as SplashScreen from 'expo-splash-screen';
+import { LocationContext } from './LocationProvider';
+import { LocationDataContext } from './LocationDataProvider'
+import { PrayerTimeContext } from './PrayerTimesProvider'
+import { QuranDataContext } from './QuranDataProvider'
+
+const LoadingContext = createContext<{ isAppReady: boolean }>({ isAppReady: false })
+
+const useLoading = () => {
+    return useContext(LoadingContext);
+  };
+
+const LoadingProvider = ({ children }: { children: ReactNode }) => {
+    const [isAppReady, setIsAppReady] = useState(false);
+    const { isLoading: isUserLocationLoading } = useContext(LocationContext);
+    const { loading: isLocationDataLoading } = useContext(LocationDataContext);
+    const { isLoading: isPrayerTimesLoading } = useContext(PrayerTimeContext);
+    const { loading: isQuranDataLoading } = useContext(QuranDataContext);
+
+    useEffect(() => {
+        const prepare = async () => {
+            try {
+                SplashScreen.preventAutoHideAsync();
+
+                while (isLocationDataLoading || isPrayerTimesLoading || isQuranDataLoading || isUserLocationLoading) {
+                    await new Promise(resolve => setTimeout(resolve, 100));
+                }
+            } catch (error) {
+                console.warn(error);
+            } finally {
+                setIsAppReady(true);
+                SplashScreen.hideAsync();
+            }
+        }
+
+        prepare();
+    }, [isLocationDataLoading, isPrayerTimesLoading, isQuranDataLoading, isUserLocationLoading])
+
+    return (
+        <LoadingContext.Provider value={{ isAppReady }}>
+            {children}
+        </LoadingContext.Provider>
+    )
+}
+
+export { LoadingContext, useLoading, LoadingProvider }
