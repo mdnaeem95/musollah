d986252e7e1ab2e5ef049e885eb7b6cd5c810369
Naeem Sani <muhdnaeem95@gmail.com>
2024-07-26 09:05:25 +0800

    Refactored the buttons on PrayerTimes tab to a single button that expands on click

---
+++ components/ExpandableButton.tsx
@@ -0,0 +1,76 @@
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
+import RoundButton from './RoundButton';
+
+interface ExpendableButtonProps {
+    onQiblatPress: () => void;
+    onDoaPress: () => void;
+    onCalendarPress: () => void;
+}
+
+const ExpandableButton = ({ onQiblatPress, onDoaPress, onCalendarPress }: ExpendableButtonProps) => {
+    const [expanded, setExpanded] = useState<boolean>(false);
+
+    const handleQiblatPress = () => {
+        setExpanded(false);
+        onQiblatPress();
+    }
+
+    const handleDoaPress = () => {
+        setExpanded(false);
+        onDoaPress();
+    }
+
+
+    return (
+        <View style={styles.container}>
+            {!expanded && (
+                <RoundButton
+                iconName='plus'
+                onPress={() => setExpanded(!expanded)}
+                size={22} 
+                />
+            )}
+            {expanded && (
+                <View style={styles.expandedButtonsContainer}>
+                    <RoundButton
+                        iconName="xmark"
+                        onPress={() => setExpanded(false)}
+                        size={22} 
+                    />
+                    <RoundButton
+                        iconName="compass"
+                        onPress={handleQiblatPress}
+                        size={22} 
+                    />
+
+                    <RoundButton
+                        iconName="hands-praying"
+                        onPress={handleDoaPress}
+                        size={22} 
+                    />
+
+                    <RoundButton 
+                        iconName="calendar-alt"
+                        onPress={onCalendarPress}
+                        size={22}
+                    />
+                </View>
+            )}
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        position: 'absolute',
+        right: 30,
+        top: 80
+    },
+    expandedButtonsContainer: {
+        gap: 40,
+      },
+
+})
+
+export default ExpandableButton
