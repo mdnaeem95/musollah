373005fb48027436c730b64275d0820b478a01d7
Naeem Sani <muhdnaeem95@gmail.com>
2024-07-30 08:16:17 +0800

    Added vibration and background colour change to compass in qiblat.

--- app/(tabs)/(prayer)/qiblat/index.tsx
+++ app/(tabs)/(prayer)/qiblat/index.tsx
@@ -1,14 +1,15 @@
-import { SafeAreaView, View } from 'react-native'
+import { View } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context';
 import React from 'react'
 import Compass from '../../../../components/Compass';
 
 const QiblatTab = () => {
   return (
-    <View style={{ height: '100%', width: '100%', flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#4D6561' }}>
+    <SafeAreaView style={{ flex: 1, backgroundColor: '#4D6561' }}>
         <View style={{ justifyContent: 'center', alignItems: 'center', flex: 1, marginBottom: 80 }}>
           <Compass />
         </View>
-    </View>
+    </SafeAreaView>
   )
 }
 
