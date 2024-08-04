831e4486df43c2f883e3f4cda2b1e0bd6daabb22
Naeem Sani <muhdnaeem95@gmail.com>
2024-08-04 19:51:40 +0800

    Added sections to the education tab using hard-coded data.

--- app/(tabs)/(education)/_layout.tsx
+++ app/(tabs)/(education)/_layout.tsx
@@ -1,40 +1,51 @@
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { Drawer } from 'expo-router/drawer';
- 
-import React from 'react'
+import React from 'react';
 import { FontAwesome6 } from '@expo/vector-icons';
 
 const EducationLayout = () => {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <Drawer>
+      <Drawer 
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#4D6561',
+          },
+          headerTitleStyle: {
+            fontFamily: 'Outfit_500Medium',
+            color: '#FFFFFF'
+          },
+          headerTintColor: '#FFFFFF',
+          headerShadowVisible: false,
+        }}
+      >
         <Drawer.Screen
-          name="index"
+          name="dashboard/index"
           options={{
             drawerLabel: 'Dashboard',
             title: 'Dashboard',
-            drawerIcon: () => <FontAwesome6 name="tachometer-alt" />
+            drawerIcon: () => <FontAwesome6 name="tachometer-alt" />,
           }}
-          />
+        />
         <Drawer.Screen
           name="courses/index"
           options={{
             drawerLabel: 'Courses',
             title: 'Courses',
-            drawerIcon: () => <FontAwesome6 name="book" />
+            drawerIcon: () => <FontAwesome6 name="book" />,
           }}
-          />
+        />
         <Drawer.Screen
           name="profile/index"
           options={{
             drawerLabel: 'Profile',
             title: 'Profile',
-            drawerIcon: () => <FontAwesome6 name="user" />
+            drawerIcon: () => <FontAwesome6 name="user" />,
           }}
-          />
+        />
       </Drawer>
     </GestureHandlerRootView>
-  )
-}
+  );
+};
 
-export default EducationLayout
+export default EducationLayout;
