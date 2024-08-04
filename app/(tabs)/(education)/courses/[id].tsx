caf8af786cb64f246975f520c2eabdf0dfd6ca0e
Naeem Sani <muhdnaeem95@gmail.com>
2024-08-04 20:05:18 +0800

    Added styling to course details.

--- app/(tabs)/(education)/courses/[id].tsx
+++ app/(tabs)/(education)/courses/[id].tsx
@@ -63,51 +63,78 @@ const CourseDetails = () => {
             <Text style={{ fontFamily: 'Outfit_600SemiBold', fontSize: 16, lineHeight: 22, color: '#FFFFFF' }}>{course.header}</Text>
         </View>
 
-        <ScrollView style={{ marginHorizontal: 16, gap: 16 }}>
+        <ScrollView style={{ marginHorizontal: 16, gap: 16 }} showsVerticalScrollIndicator={false}>
             <View style={{ flexDirection: 'row', gap: 10 }}>
                 <View style={{ alignContent: 'center', justifyContent: 'center' }}>
                     <Image style={{ width: 48, height: 48, backgroundColor: '#FFFFFF', borderRadius: 24 }} />
                 </View>
                 <View style={{ alignContent: 'center', justifyContent: 'center', gap: 5 }}>
-                    <Text>Author</Text>
-                    <Text>Ryan Gosling</Text>
+                    <Text style={styles.subText}>Author</Text>
+                    <Text style={styles.mainText}>Ryan Gosling</Text>
                 </View>
             </View>
 
             <View style={{ marginTop: 16, gap: 10 }}>
-                <View style={{ flexDirection: 'row', gap: 10 }}>
-                    <FontAwesome6 name="file" regular />
-                    <Text>{`${course.modules.length} Modules`}</Text>
+                <View style={{ flexDirection: 'row', gap: 10, alignItems: 'center' }}>
+                    <FontAwesome6 name="file" regular color='white' size={20} />
+                    <Text style={styles.contentText}>{`${course.modules.length} Modules`}</Text>
                 </View>
-                <View style={{ flexDirection: 'row', gap: 8 }}>
-                    <FontAwesome6 name="clock" regular />
-                    <Text>{`${course.modules.length} Hours estimated time to finish`}</Text>
+                <View style={{ flexDirection: 'row', gap: 8, alignItems: 'center' }}>
+                    <FontAwesome6 name="clock" regular color='white' size={20} />
+                    <Text style={styles.contentText}>{`${course.modules.length} Hours estimated time to finish`}</Text>
                 </View>
             </View>
 
             <View style={{ marginTop: 16, gap: 10 }}>
-                <Text>Description</Text>
-                <Text>{course.description}</Text>
+                <Text style={styles.subText}>Description</Text>
+                <Text style={styles.contentText}>{course.description}</Text>
             </View>
 
             <View style={{ marginTop: 16, gap: 10 }}>
-                <Text>Modules</Text>
+                <Text style={styles.subText}>Modules</Text>
                 {course.modules.map((module, index) => (
                     <View key={index} style={{ width: '100%', backgroundColor: '#FFFFFF', borderRadius: 10, padding: 10, gap: 4 }}>
-                        <Text>{module}</Text>
-                        <Text>1 hour</Text>
+                        <Text style={[styles.subText, { color: '#000000' }]}>{`${index + 1}. ${module}`}</Text>
+                        <Text style={[styles.contentText, { color: '#000000' }]}>1 hour</Text>
                     </View>
                 ))}
             </View>
 
             <View style={{ marginTop: 16 }}>
                 <TouchableOpacity style={{ width: '100%', backgroundColor: '#FFFFFF', borderRadius: 10, padding: 20, alignItems: 'center' }}>
-                    <Text>Start Learning</Text>
+                    <Text style={styles.btnText}>Start Learning</Text>
                 </TouchableOpacity>
             </View>
         </ScrollView>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+    subText: {
+        fontFamily: 'Outfit_500Medium',
+        fontSize: 14,
+        lineHeight: 18,
+        color: '#FFFFFF'
+    },
+    mainText: {
+        fontFamily: 'Outfit_500Medium',
+        fontSize: 16,
+        lineHeight: 21,
+        color: '#FFFFFF'
+    },
+    contentText: {
+        fontFamily: 'Outfit_400Regular',
+        fontSize: 14,
+        lineHeight: 18,
+        color: '#FFFFFF'
+    },
+    btnText: {
+        fontFamily: 'Outfit_500Medium', 
+        fontSize: 16,
+        lineHeight: 22,
+        color: '#000000'
+    }
+})
 
 export default CourseDetails;
