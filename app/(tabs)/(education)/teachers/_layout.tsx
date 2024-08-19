5945866642564631a1d636887a1a2c996d1465bc
Naeem Sani <muhdnaeem95@gmail.com>
2024-08-19 16:58:23 +0800

    Added more options to the settings.

---
+++ app/(tabs)/(education)/teachers/_layout.tsx
@@ -0,0 +1,11 @@
+import { Stack } from "expo-router";
+
+const TeachersLayout = () => {
+  return (
+    <Stack screenOptions={{
+      headerShown: false
+    }} />
+  )
+}
+
+export default TeachersLayout
