759f74f780af93743d2ed2b95a853e6d62db772c
Naeem Sani <muhdnaeem95@gmail.com>
2024-08-22 09:57:18 +0800

    Fixed the prayer times not updating when another date was selected from calendar.

--- api/prayers/index.ts
+++ api/prayers/index.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { formatDateForAPI } from '../../utils';
 
 export const fetchPrayerTimes = async () => {
     try {
@@ -18,14 +19,16 @@ export const fetchPrayerTimes = async () => {
 
 export const fetchTimesByDate = async (date: string) => {
     try {
-        const response = await fetch(`http://api.aladhan.com/v1/timingsByCity?${date}&city=Singapore&country=Singapore&method=11`)
+        const formattedDate = formatDateForAPI(date);
+        console.log(formattedDate)
+        const response = await fetch(`http://api.aladhan.com/v1/timings/${formattedDate}?latitude=1.290270&longitude=103.851959&method=11`)
 
         if (!response.ok) {
             throw new Error(`Failed to fetch prayer times for date ${date}`)
         }
 
         const data = await response.json();
-        return data;
+        return data
     } catch (error) {
         console.error (`Error fetching prayer times for ${date}: `, error)
     }
