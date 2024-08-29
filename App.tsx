d33b19ddf8e743d99affc8fd6535d772d546b6f8
Naeem Sani <muhdnaeem95@gmail.com>
2024-08-29 23:06:16 +0800

    Added sentry set up.

--- App.tsx
+++ App.tsx
@@ -2,8 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import 'expo-dev-client';
 import React from 'react';
+import * as Sentry from '@sentry/react-native';
 
 export default function App() {
+  Sentry.init({
+    dsn: 'https://6273527f044ad3d726f911727730fdf7@o4507860915257344.ingest.us.sentry.io/4507860932034560',
+    debug: true,
+  })
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
