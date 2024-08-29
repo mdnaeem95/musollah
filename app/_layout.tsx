23221dc8ca113240a5e179045f0560c9d5ea57cb
Naeem Sani <muhdnaeem95@gmail.com>
2024-08-29 23:06:16 +0800

    Added sentry set up.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,10 +3,12 @@ import { Provider } from 'react-redux';
 import store from '../redux/store/store';
 import RootLayout from './RootLayout'; // Adjust the path as necessary
 
+import * as Sentry from '@sentry/react-native'
+
 const AppLayout = () => (
   <Provider store={store}>
     <RootLayout />
   </Provider>
 );
 
-export default AppLayout;
+export default Sentry.wrap(AppLayout);
