34bc6821192116a3748087db2d12ca35bbd98765
Naeem Sani <muhdnaeem95@gmail.com>
2024-09-03 08:49:09 +0800

    Removed sentry for now.

--- app/RootLayout.tsx
+++ app/RootLayout.tsx
@@ -19,22 +19,22 @@ import { fetchSurahsData } from '../redux/slices/quranSlice'
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import AuthScreen from './(auth)/AuthScreen';
 
-import * as Sentry from '@sentry/react-native';
+// import * as Sentry from '@sentry/react-native';
 
 SplashScreen.preventAutoHideAsync();
 
-const routingInstrumentation = new Sentry.ReactNavigationInstrumentation();
+// const routingInstrumentation = new Sentry.ReactNavigationInstrumentation();
 
-Sentry.init({
-  dsn: 'https://6273527f044ad3d726f911727730fdf7@o4507860915257344.ingest.us.sentry.io/4507860932034560',
-  debug: true,
-  integrations: [
-    new Sentry.ReactNativeTracing({
-      routingInstrumentation,
-      enableNativeFramesTracking: !isRunningInExpoGo(),
-    })
-  ]
-})
+// Sentry.init({
+//   dsn: 'https://6273527f044ad3d726f911727730fdf7@o4507860915257344.ingest.us.sentry.io/4507860932034560',
+//   debug: true,
+//   integrations: [
+//     new Sentry.ReactNativeTracing({
+//       routingInstrumentation,
+//       enableNativeFramesTracking: !isRunningInExpoGo(),
+//     })
+//   ]
+// })
 
 const RootLayout = () => {
   const dispatch = useDispatch<AppDispatch>();
@@ -49,11 +49,11 @@ const RootLayout = () => {
 
   const ref = useNavigationContainerRef();
 
-  useEffect(() => {
-    if (ref) {
-      routingInstrumentation.registerNavigationContainer(ref);
-    }
-  }, [ref])
+  // useEffect(() => {
+  //   if (ref) {
+  //     routingInstrumentation.registerNavigationContainer(ref);
+  //   }
+  // }, [ref])
 
   const [fontsLoaded] = useFonts({
     Outfit_300Light,
@@ -134,4 +134,4 @@ const RootLayout = () => {
   );
 }
 
-export default Sentry.wrap(RootLayout);
+export default RootLayout;
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,9 +3,6 @@ import { Provider } from 'react-redux';
 import store from '../redux/store/store';
 import RootLayout from './RootLayout'; // Adjust the path as necessary
 
-import * as Sentry from '@sentry/react-native'
-import { useNavigationContainerRef } from 'expo-router';
-
 const AppLayout = () => (
   <Provider store={store}>
     <RootLayout />
