530eaae0c944d6a39d7680df04f3eaffbdaaf9e4
Naeem Sani <muhdnaeem95@gmail.com>
2024-09-05 21:37:35 +0800

    Added a loading screen.

---
+++ components/LoadingScreen.tsx
@@ -0,0 +1,70 @@
+import { View, Text, Animated, ImageBackground, StyleSheet, ActivityIndicator, Image } from 'react-native'
+import React, { useEffect, useState } from 'react'
+
+interface LoadingScreenProps {
+    message?: string;
+}
+
+const LoadingScreen = ({ message = 'Setting up the app...' }: LoadingScreenProps) => {
+    const [fadeAnim] = useState(new Animated.Value(0));
+
+    useEffect(() => {
+        Animated.loop(
+          Animated.sequence([
+            Animated.timing(fadeAnim, {
+              toValue: 1,
+              duration: 1500,
+              useNativeDriver: true,
+            }),
+            Animated.timing(fadeAnim, {
+              toValue: 0,
+              duration: 1500,
+              useNativeDriver: true,
+            }),
+          ])
+        ).start();
+      }, [fadeAnim]);
+
+    return (
+    <View style={styles.container}>
+        {/* App Logo */}
+        <Image
+        source={require('../assets/rihlahLogo.svg')} // Replace with your app's logo path
+        style={styles.logo}
+        resizeMode="contain"
+        />
+
+        {/* Activity Indicator */}
+        <ActivityIndicator size="large" color="#4D6561" style={styles.spinner} />
+
+        {/* Animated Text */}
+        <Animated.Text style={[styles.loadingText, { opacity: fadeAnim }]}>
+        {message}
+        </Animated.Text>
+    </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center', // Center content vertically
+        alignItems: 'center', // Center content horizontally
+        backgroundColor: '#FFFFFF', // White background for clean appearance
+      },
+      logo: {
+        width: 150, // Adjust size as needed
+        height: 150, // Adjust size as needed
+        marginBottom: 30, // Space between logo and spinner
+      },
+      spinner: {
+        marginBottom: 20, // Space between spinner and text
+      },
+      loadingText: {
+        fontSize: 18,
+        fontFamily: 'Outfit_600SemiBold',
+        color: '#4D6561',
+      },
+})
+
+export default LoadingScreen
