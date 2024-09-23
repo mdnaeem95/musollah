e97ec6a2fc0d80820058419b34ec85a8fb3af9ca
Naeem Sani <muhdnaeem95@gmail.com>
2024-09-23 14:07:00 +0800

    upgraded firebase deps

--- App.tsx
+++ App.tsx
@@ -2,7 +2,6 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import 'expo-dev-client';
 import React from 'react';
-import * as Sentry from '@sentry/react-native';
 
 export default function App() {
   return (
