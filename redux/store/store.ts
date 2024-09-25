49c3532163c0acaa1a951da4d16ae02b2882d265
Naeem Sani <muhdnaeem95@gmail.com>
2024-09-25 09:42:22 +0800

    Set up redux persist for the store.

--- redux/store/store.ts
+++ redux/store/store.ts
@@ -1,5 +1,4 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { thunk } from 'redux-thunk';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import userLocationReducer from '../slices/userLocationSlice';
 import prayerReducer  from '../slices/prayerSlice';
 import musollahReducer from '../slices/musollahSlice';
@@ -7,24 +6,40 @@ import quranReducer from '../slices/quranSlice'
 import userReducer from '../slices/userSlice'
 import dashboardReducer from '../slices/dashboardSlice';
 import courseReducer from '../slices/courseSlice';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { FLUSH, PAUSE, PERSIST, persistReducer, persistStore, PURGE, REGISTER, REHYDRATE } from 'redux-persist';
+
+// Configuration for redux-persist
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage
+}
+
+// Combining all reducers
+const rootReducer = combineReducers({
+  location: userLocationReducer,
+  prayer: prayerReducer,
+  musollah: musollahReducer,
+  quran: quranReducer,
+  user: userReducer,
+  dashboard: dashboardReducer,
+  course: courseReducer,
+})
+
+// Wrap root reducer with persistreducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
-  reducer: {
-    location: userLocationReducer,
-    prayer: prayerReducer,
-    musollah: musollahReducer,
-    quran: quranReducer,
-    user: userReducer,
-    dashboard: dashboardReducer,
-    course: courseReducer,
-  },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware(
-    { immutableCheck: false, 
-      serializableCheck: false,
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER] 
     }
-  ).concat(thunk),
+  })
 });
+
+const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
-export default store;
+export { store, persistor };
