857302032e008b2dce84c4b0ee7a1214aa2d2681
Naeem Sani <muhdnaeem95@gmail.com>
2024-09-25 09:42:22 +0800

    Set up redux persist for the store.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import * as Notifications from "expo-notifications"
-import store from '../redux/store/store';
+import { store, persistor } from '../redux/store/store';
 import { NotificationProvider } from "../context/NotificationContext"
 import RootLayout from './RootLayout'; // Adjust the path as necessary
+import { PersistGate } from 'redux-persist/integration/react';
 
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
@@ -16,7 +17,9 @@ Notifications.setNotificationHandler({
 const AppLayout = () => (
   <NotificationProvider>
     <Provider store={store}>
-      <RootLayout />
+      <PersistGate persistor={persistor}>
+        <RootLayout />
+      </PersistGate>
     </Provider>
   </NotificationProvider>
 );
