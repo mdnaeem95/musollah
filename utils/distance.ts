0273859ad0b19c12ad0dab6223f79216faaa18a9
Naeem Sani <muhdnaeem95@gmail.com>
2024-12-18 11:45:06 +0800

    Changed restaurant view on the landing page to include ratings, reviews, and distance.

--- utils/distance.ts
+++ utils/distance.ts
@@ -16,3 +16,17 @@ export const getDistanceFromLatLonInKm = (lat1: number, lon1: number, lat2: numb
     return deg * (Math.PI / 180);
   };
   
+export const haversineDistance = (coords1: { latitude: number, longitude: number }, coords2: { latitude: number, longitude: number }): number => {
+  const toRadians = (degree: number) => (degree * Math.PI) / 180;
+
+  const R = 6371;
+  const dLat = toRadians(coords2.latitude - coords1.latitude)
+  const dLon = toRadians(coords2.longitude - coords1.longitude)
+  const lat1 = toRadians(coords1.latitude)
+  const lat2 = toRadians(coords2.latitude)
+
+  const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.cos(lat1) * Math.cos(lat2) * Math.sin(dLon / 2) * Math.sin(dLon /2);
+
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  return R * c;
+}
