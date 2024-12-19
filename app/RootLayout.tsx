f7e341ee56b1988b7a7fb129a80d69f62216b5c1
Naeem Sani <muhdnaeem95@gmail.com>
2024-12-19 14:18:35 +0800

    removed (auth) as dont'require it anymore

---
+++ app/RootLayout.tsx
@@ -0,0 +1,151 @@
+import React, { useCallback, useEffect, useState } from 'react';
+import { Slot, Stack, useRouter } from 'expo-router';
+import { useDispatch } from 'react-redux';
+import Purchases from 'react-native-purchases';
+import * as SplashScreen from 'expo-splash-screen';
+import { AppState, Platform, StyleSheet, View } from 'react-native';
+import { Outfit_300Light, Outfit_400Regular, Outfit_500Medium, Outfit_600SemiBold, Outfit_700Bold } from "@expo-google-fonts/outfit";
+import { Amiri_400Regular } from "@expo-google-fonts/amiri";
+import { useFonts } from 'expo-font';
+import { AppDispatch, persistor } from '../redux/store/store';
+import { useLogTrackPlayerState } from '../hooks/useLogTrackPlayerState'
+import { useSetupTrackPlayer } from "../hooks/useSetupTrackPlayer"
+import { fetchPrayerTimesData } from '../redux/slices/prayerSlice';
+import { fetchSurahsData } from '../redux/slices/quranSlice';
+import { getAuth, onAuthStateChanged } from '@react-native-firebase/auth';
+import { fetchDailyDoasData } from '../redux/slices/doasSlice';
+import LoadingScreen from '../components/LoadingScreen';
+import Animated, { Easing, useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated';
+import { listenForUserUpdates } from '../redux/slices/userSlice';
+import TrackPlayer from 'react-native-track-player';
+import { playbackService } from '../constants/playbackService';
+if (AppState.currentState === 'active') {
+  SplashScreen.preventAutoHideAsync();
+}
+TrackPlayer.registerPlaybackService(() => playbackService)
+
+const RootLayout = () => {
+  const router = useRouter();
+  const dispatch = useDispatch<AppDispatch>();  
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+  const [isEssentialDataFetched, setIsEssentialDataFetched] = useState<boolean>(false);
+  const [isTrackPlayerSetup, setIsTrackPlayerSetup] = useState(false);
+  const [isNonEssentialDataFetched, setIsNonEssentialDataFetched] = useState<boolean>(false);
+  const [isFontsLoaded] = useFonts({
+    Outfit_300Light,
+    Outfit_500Medium,
+    Outfit_400Regular,
+    Outfit_600SemiBold,
+    Outfit_700Bold,
+    Amiri_400Regular,
+  });
+  const isRehydrated = persistor.getState().bootstrapped;
+  const translateY = useSharedValue(0);
+  const hideSplashScreen = useCallback(() => {
+    translateY.value = withTiming(1000, { duration: 300, easing: Easing.inOut(Easing.ease) })
+  }, [])
+  // Monitor Authentication State
+  useEffect(() => {
+    const auth = getAuth();
+    const unsubscribe = onAuthStateChanged(auth, (user: any) => {
+      setIsAuthenticated(!!user);
+    });
+    return () => unsubscribe();
+  }, []);
+  
+  // Listener to monitor changes in user documents
+  useEffect(() => {
+    dispatch(listenForUserUpdates());
+  }, [dispatch]);
+  // Fetch only essential data (Prayer Times) first
+  useEffect(() => {
+    if (!isRehydrated) return;
+    const fetchEssentialData = async () => {
+      try {
+        console.log('Fetching essential data: Prayer Times...');
+        await dispatch(fetchPrayerTimesData()).unwrap();
+        setIsEssentialDataFetched(true);
+      } catch (error) {
+        console.error('Error fetching essential data:', error);
+      }
+    };
+    if (!isEssentialDataFetched) {
+      fetchEssentialData();
+    }
+  }, [dispatch, isEssentialDataFetched, isRehydrated]);
+  // Fetch some non-essential data (surahs) after the app has loaded but defer fetching user location and musollah data until necessary
+  useEffect(() => {
+      const fetchNonEssentialData = async () => {
+        try {
+          console.log('Fetching non-essential data (surahs)...');
+          await dispatch(fetchSurahsData()).unwrap();
+          await dispatch(fetchDailyDoasData()).unwrap();
+          setIsNonEssentialDataFetched(true);
+        } catch (error) {
+          console.error('Error fetching non-essential data:', error);
+        }
+      };
+
+      if (!isNonEssentialDataFetched) {
+        fetchNonEssentialData();
+      }
+  }, [dispatch, isNonEssentialDataFetched]);
+
+  const handleTrackPlayerLoaded = useCallback(() => {
+    setIsTrackPlayerSetup(true);
+  }, [])
+  useSetupTrackPlayer({
+    onLoad: handleTrackPlayerLoaded
+  })
+  useLogTrackPlayerState();
+  // Hide the splash screen once fonts and essential data are ready
+  useEffect(() => {
+    const hideSplashScreenandAnimate = async () => {
+      try {
+        hideSplashScreen();
+        await SplashScreen.hideAsync();
+      } catch (error) {
+        console.error('Error hiding SplashScreen:', error);
+      }
+    };
+    if (isFontsLoaded && isEssentialDataFetched && isTrackPlayerSetup)  {
+      hideSplashScreenandAnimate();
+    }
+  }, [isFontsLoaded, isEssentialDataFetched, hideSplashScreen, isTrackPlayerSetup]);
+  // reanimated style for splash screen sliding animation
+  const animatedSplashStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{ translateY: translateY.value }],
+    }
+  })
+
+  if (!isFontsLoaded || !isEssentialDataFetched || !isRehydrated) {
+    return (
+      <View style={{ flex: 1 }}>
+        <LoadingScreen message="Setting up the app..." />
+        <Slot />
+      </View>
+    );
+  }
+  
+  // Main app layout after authentication and data fetch
+  return (
+    <View style={{ flex: 1 }}>
+      {/* SplashScreen */}
+      <Animated.View style={[styles.splashScreen, animatedSplashStyle]} />
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false, gestureEnabled: false }} redirect />
+      </Stack>
+    </View>
+  );
+}
+
+const styles = {
+  splashScreen: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: '#2E3D3A',
+    zIndex: 1
+  }
+}
+
+export default RootLayout;
--- providers/AppProviders.tsx
+++ providers/AppProviders.tsx
@@ -1,40 +1,22 @@
 import React from 'react';
 import { Provider as ReduxProvider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-import { NotificationProvider } from '../context/NotificationContext';
-import { ThemeProvider } from '../context/ThemeContext';
-import { ActionSheetProvider } from '@expo/react-native-action-sheet';
 import { store, persistor } from '../redux/store/store';
-import LoadingScreen from '../components/LoadingScreen';
-import Toast from 'react-native-toast-message';
-import { toastConfig } from '../utils/toastConfig';
-import * as Notifications from 'expo-notifications';
-
-Notifications.setNotificationHandler({
-  handleNotification: async () => ({
-    shouldShowAlert: true,
-    shouldPlaySound: false,
-    shouldSetBadge: false,
-  }),
-});
+import { ThemeProvider } from '../context/ThemeContext';
+import { NotificationProvider } from '../context/NotificationContext';
+import { ActionSheetProvider } from '@expo/react-native-action-sheet';
 
 const AppProviders: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
-    <ActionSheetProvider>
-      <ThemeProvider>
-        <NotificationProvider>
-          <ReduxProvider store={store}>
-            <PersistGate
-              persistor={persistor}
-              loading={<LoadingScreen message="Setting up the app..." />}
-            >
-              {children}
-              <Toast config={toastConfig} />
-            </PersistGate>
-          </ReduxProvider>
-        </NotificationProvider>
-      </ThemeProvider>
-    </ActionSheetProvider>
+    <ReduxProvider store={store}>
+      <PersistGate persistor={persistor} loading={null}>
+        <ActionSheetProvider>
+          <ThemeProvider>
+            <NotificationProvider>{children}</NotificationProvider>
+          </ThemeProvider>
+        </ActionSheetProvider>
+      </PersistGate>
+    </ReduxProvider>
   );
 };
 
