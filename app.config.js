80b3b1a4579641ed84d7ef879702f84f430aaeba
Naeem Sani <muhdnaeem95@gmail.com>
2024-12-21 15:22:33 +0800

    added LSApplicationQueriesScehemes to infoPlist.

--- app.config.js
+++ app.config.js
@@ -19,7 +19,7 @@ export default {
       "supportsTablet": false,
       "infoPlist": {
         "UIBackgroundModes": ["location", "fetch", "remote-notification", "audio"],
-        "LSApplicationQueriesSchemes": "file",
+        "LSApplicationQueriesSchemes": ["file", "tel"],
         "ITSAppUsesNonExemptEncryption": false,
         "NSLocationWhenInUseUsageDescription": "We use your location to show you relevant data.",
         "NSLocationAlwaysUsageDescription": "We use your location to show you relevant data even when the app is in the background."
